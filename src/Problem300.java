ae083ca21e021da445a2f8471f68686bec613f6d
vincent zhou <grayy921013@gmail.com>
2016-04-15 18:27:54 +0800

    300 finished

---
+++ src/Problem300.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+/**
+ * Created by Zhehui Zhou on 4/15/16.
+ */
+public class Problem300 {
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        int len = 0;
+
+        for(int x : nums) {
+            int i = Arrays.binarySearch(dp, 0, len, x);
+            if(i < 0) i = -(i + 1);
+            dp[i] = x;
+            if(i == len) len++;
+        }
+
+        return len;
+    }
+
+}
